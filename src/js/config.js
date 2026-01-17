43ea253f5d823d9924d740c7337ac83abb2fadbf
divergentinc2021 <laurent@divergentbiz.com>
2026-01-17 22:12:23 +0200

    Added more security and implemented better UI flow

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // Apps Script API URL - Replace with your deployment URL
-export const API_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbzGSfmG-uWUd-9WYc99JcBDcekFb4A8D4lv1JqKRUIHx3fI06dt4xd3R5oHR9Nm-gzfTA/exec';
 
 // App URL (auto-detected)
 export const APP_URL = window.location.origin;
